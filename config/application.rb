a4cd88cf8940795df8992802055ccf2e396f61fd
Alex Kozlov <arcadiabakery@gmail.com>
2013-06-30 21:00:33 -0700

    precompile

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,7 @@ if defined?(Bundler)
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
-  #for the use in Heroku
-  #config.assets.initialize_on_precompile = false
+  
 end
 
 module Bute
@@ -60,5 +59,8 @@ module Bute
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    #for the use in Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
