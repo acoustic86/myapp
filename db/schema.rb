c528efccc3e413aa790b5b5c1f21999d0d8515af
Alex Kozlov <arcadiabakery@gmail.com>
2013-07-16 18:23:33 -0700

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130701030155) do
 
-  create_table "products", :force => true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.integer  "price"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
   create_table "profiles", :force => true do |t|
     t.string   "name"
     t.text     "about_me"
@@ -29,11 +21,6 @@ ActiveRecord::Schema.define(:version => 20130701030155) do
     t.string   "address"
     t.string   "profession"
     t.string   "image_url"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "services", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
